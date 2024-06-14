5630b7bc796f477d534fc80a78a427eee8aa61e8
padak <diseor1701@naver.com>
2024-06-14 17:17:00 +0900

    장치 초기화

--- GameCoding/GameCoding/Game.h
+++ GameCoding/GameCoding/Game.h
@@ -10,7 +10,16 @@ public:
 	void Render();
 
 private:
+	void RenderBegin();
+	void RenderEnd();
+private:
 	void CreateDeviceAndSwapChain();
+	void CreateRenderTargetView();
+	// 만든 후면 버퍼를 묘사하는 존재
+	// View : 어떤 자원에서 대해서 태그를 달아가지고 
+	// 나중에 GPU에 친절하게 설명하기 위한 부연 설명같은 것
+
+	void SetViewport();
 
 private:
 	HWND _hwnd;
@@ -18,9 +27,16 @@ private:
 	uint32 _height = 0;
 
 private:
-	// DX
+	// Device & SwapChain
 	ComPtr<ID3D11Device> _device; // 스마트포인터
 	ComPtr<ID3D11DeviceContext> _deviceContext;
+	ComPtr<IDXGISwapChain> _swapChain;
+	
+	// RTV
+	ComPtr<ID3D11RenderTargetView> _renderTargetView;
+	D3D11_VIEWPORT _viewport = { 0 }; // 화면을 묘사하는 구조체
+
+	float _clearColor[4] = { 0.5f, 0.5f, 1.5f, 0.5f };
 };
 
  
